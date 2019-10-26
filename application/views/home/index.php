0d13219896b553837263cf25a10d3dfce2455396
dicky Ardianto <dikiardianto19@gmail.com>
2019-10-26 14:26:28 +0700

    Menambahkan foto baru pada tampilan Home

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -1,3 +1,11 @@
 <div class="container">
     <h1>Hello, ini Website <?= $nama ?> !</h1>
+    <div class="card" style="width: 18rem;">
+        <img src="<?= base_url() ?>assets/img/d.jpg" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">Dicky Ardianto</h5>
+            <p class="card-text">173040046</p>
+            <a href="https://www.instagram.com/ardianto.dicky/" target="_blank" class="btn btn-primary">About Me</a>
+        </div>
+    </div>
 </div>
