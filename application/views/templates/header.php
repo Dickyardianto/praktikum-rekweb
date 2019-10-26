cad67700f90129807156cb5cbf91b981e313b294
dicky Ardianto <dikiardianto19@gmail.com>
2019-10-26 14:14:47 +0700

    memodifikasi templates header.php

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -14,7 +14,7 @@
 
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container">
-            <a class="navbar-brand" href="#">CI App</a>
+            <a class="navbar-brand" href="<?= base_url(); ?>">CI App</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup"
                 aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
